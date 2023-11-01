a4c6840ac3153057561ef0ddaa57a477904f61f7
Toan Nguyen <nguyenvantoan263@gmail.com>
2023-11-02 00:18:36 +0800

    update component product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CheckoutFormComponent } from './components/checkout-form/checkout-form.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CheckoutFormComponent
+    CheckoutFormComponent,
+    ProductItemComponent,
+    ProductListComponent,
+    ProductItemDetailComponent
   ],
   imports: [
     BrowserModule,
