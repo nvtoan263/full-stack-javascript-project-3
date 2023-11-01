f68fe1f255132b9873a9241214d91abb2a59a26b
Toan Nguyen <nguyenvantoan263@gmail.com>
2023-11-02 00:35:18 +0800

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutFormComponent } from './components/checkout-form/checkout-form.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'product/:id', component: ProductItemDetailComponent },
+  { path: 'cart', component: CartComponent },
+  {
+    path: 'success/:firstName/:totalPrice',
+    component: CheckoutFormComponent,
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
